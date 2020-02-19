d00b68b65640e192c5ad318c071d672ea89a35c5
Alex <alexandr.dubiaga@gmail.com>
2020-02-19 09:39:30 +0200

    Updates

---
+++ precache-manifest.5960c80a2fd4f291ee823227bd363113.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc695ee43bb58375df8b6d1299748f02",
+    "url": "/TypeScript/index.html"
+  },
+  {
+    "revision": "b3fa2fa603a9acdf363e",
+    "url": "/TypeScript/static/css/main.b9e63708.chunk.css"
+  },
+  {
+    "revision": "89cb0e0756e767e85c24",
+    "url": "/TypeScript/static/js/2.34fb009e.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/TypeScript/static/js/2.34fb009e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3fa2fa603a9acdf363e",
+    "url": "/TypeScript/static/js/main.fe26258a.chunk.js"
+  },
+  {
+    "revision": "b8eeadfdea02e5e7981b",
+    "url": "/TypeScript/static/js/runtime-main.d4446b45.js"
+  }
+]);
